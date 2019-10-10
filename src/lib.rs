ad312f157d7ee26a580ecf34f8afc94890b2f448
Daniel F. Maxwell-Ross <daniel.maxwell-ross@carbonlighthouse.com>
2019-10-09 22:26:50 -0700

    hello with sexy iterative bigint function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+extern crate num;
+use num::BigUint;
+use std::ops::Add;
+
 mod utils;
 
 use wasm_bindgen::prelude::*;
@@ -10,10 +14,29 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 extern {
-    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn debug(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn error(s: &str);
+}
+
+
+pub fn fib(n: usize) -> BigUint {
+    let mut i1 = BigUint::from(0usize); let mut i2 = BigUint::from(1usize);
+    let mut scratch;
+
+    for x in 0..n {
+        scratch = i1.add(&i2);
+        i1 = i2;
+        i2 = scratch;
+    }
+
+    i1
 }
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, rust-wasm-hello!");
+    log(&format!("{}", fib(5000)))
 }
