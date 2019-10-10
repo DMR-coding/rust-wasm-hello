9519d2313baad3d706a2e26e51e4897dd5fb31b9
Daniel F. Maxwell-Ross <daniel.maxwell-ross@carbonlighthouse.com>
2019-10-10 11:32:31 -0700

    toggle racing with JS

--- www/index.js
+++ www/index.js
@@ -16,19 +16,22 @@ const go_button = document.querySelector("#go_btn");
 const iterations_box = document.querySelector("#iterations");
 const time_output = document.querySelector("#time_output");
 const value_output = document.querySelector("#value_output");
+const race_checkbox = document.querySelector("#race");
 
 go_button.addEventListener("click", function(){
     const iterations = parseInt(iterations_box.value);
+    const race = race_checkbox.checked;
 
     const start = performance.now();
     const rust_val = wasm.fib_s(iterations);
     let rust = performance.now();
-    const _ = fib(iterations);
-    let native = performance.now();
+    if(race) {
+        const _ = fib(iterations);
+        let native = performance.now();
+        time_output.textContent = `Rust took: ${rust - start}\nJavascript Took: ${native - rust}`;
+    } else {
+        time_output.textContent = `Rust took: ${rust - start}`
+    }
 
-    const rust_time = rust - start;
-    const native_time = native - rust;
-    
-    time_output.textContent = `Rust took: ${rust_time}\nJavascript Took: ${native_time}`;
     value_output.textContent = rust_val;
 });
